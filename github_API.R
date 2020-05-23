e9b047fb50ae3ae0c0f8493db2afc70f0c52b5a5
Ante Pettersson <ante.pettersson@gmail.com>
2020-05-22 23:51:08 -0400

    github_API.R: authentication works, list obtained

--- github_API.R
+++ github_API.R
@@ -2,7 +2,7 @@ library(httr)
 
 oauth_endpoints("github")
 
-myapp <- oauth_app("github",
+myapp <- oauth_app("Coursera R API usage homework script",
                    key = "878ca0013a8c06406965",
                    secret = "806eac67ee5af01591e4cb1234c9751a7c23a26c"
 )
@@ -10,6 +10,6 @@ myapp <- oauth_app("github",
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/rate_limit", gtoken)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
 stop_for_status(req)
-content(req)
+obj <- content(req)
