bef25e25c1d7043202f494b840e298d037002c10
Ante Pettersson <ante.pettersson@gmail.com>
2020-05-22 22:20:23 -0400

    github_API.R : first commit. problems with callback

---
+++ github_API.R
@@ -0,0 +1,15 @@
+library(httr)
+
+oauth_endpoints("github")
+
+myapp <- oauth_app("github",
+                   key = "878ca0013a8c06406965",
+                   secret = "806eac67ee5af01591e4cb1234c9751a7c23a26c"
+)
+
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/rate_limit", gtoken)
+stop_for_status(req)
+content(req)
